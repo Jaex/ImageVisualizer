709abf23de545debbc18ed826178a135ed56fb1d
Jaex <Jaex@users.noreply.github.com>
2014-07-29 10:34:07 +0300

    Moved project folders to parent folder

--- ImageVisualizerTest/Properties/AssemblyInfo.cs
+++ ImageVisualizerTest/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImageVisualizer")]
+[assembly: AssemblyTitle("ImageVisualizerTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jaex")]
-[assembly: AssemblyProduct("ImageVisualizer")]
-[assembly: AssemblyCopyright("Copyright © Jaex")]
+[assembly: AssemblyProduct("ImageVisualizerTest")]
+[assembly: AssemblyCopyright("Copyright (C) Jaex")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8ae434d3-a035-41f8-9cc1-4a5ce513ee7c")]
+[assembly: Guid("bb232303-8977-4b7b-a7f8-5f7f75f038a4")]
 
 // Version information for an assembly consists of the following four values:
 //
